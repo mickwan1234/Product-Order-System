5ada6ea5b65e454c3e0d1d7002813b7c21c86b53
Nghia, Nguyen <Nghia.Nguyen1@ge.com>
2022-06-30 16:39:07 +0700

    fix order quantity bug

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CartService } from 'src/cart/cart.service';
+import Product from 'src/product/product.entity';
 import { Repository } from 'typeorm';
 import Order from './order.entity';
 
@@ -8,6 +9,8 @@ import Order from './order.entity';
 export class OrderService {
     @InjectRepository(Order)
     private orderRepository: Repository<Order>;
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>;
 
     constructor(private cartService: CartService){}
 
@@ -15,6 +18,20 @@ export class OrderService {
         const cart = await this.cartService.getCartInfo(userId);
         const cartJson = JSON.stringify(cart.cartItems)
         const paymentMethod = req.body.paymentMethod;
+
+        for (let index = 0; index < cart.cartItems.length; index++) {
+            const cartItem = cart.cartItems[index];
+            const item = await this.productRepository.findOneBy({
+                id: cartItem.productId
+            })
+
+            item.quantity = item.quantity - cartItem.quantity;
+            if(item.quantity < 0){
+                throw new BadRequestException('Can\'t create order with quantity greater than inventory')
+            }
+            this.productRepository.save(item);
+
+        }
         
         const order = new Order(userId, cartJson,cart.totalPrice,paymentMethod);
 
